2135cf9724aa70414eeaaf14fc52289a725491f3
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-08-21 18:20:57 +0800

    Auto commit

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fyne.io/fyne/v2/widget"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
 
 type config struct {
+	CurrentFile   fyne.URI
 	EditWidget    *widget.Entry
 	PreviewWidget *widget.RichText
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+
 }
